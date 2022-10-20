deddc966c3b23b9aa2da518ab78bd59ecb0f1481
kiralx <kiralx@bk.ru>
2022-10-20 16:49:20 +0300

    added structure in program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
 ﻿// https://gbcdn.mrgcdn.ru/uploads/asset/4312773/attachment/ed8c1f2c15da325114976e1c313ef5f8.png
+
+string[] arrayIn = GetStrArr();
+string[] arrayOut = GetStrArrLenghtLower3(arrayIn);
+PrintArray(arrayOut);
+
+
+string[] GetStrArr()
+{
+
+}
+
+string[] GetStrArrLenghtLower3(string[] arr)
+{
+
+}
+
+
+void PrintArray(string[] arr){
+    Console.WriteLine("[" + String.Join(", ", arr) + "]");
+}
